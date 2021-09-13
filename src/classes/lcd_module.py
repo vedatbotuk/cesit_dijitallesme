c3e3e17d89bbceda47c81a336d2cdf16cc0fc571
vedat <vedat.botuk@gmail.com>
2021-09-13 12:04:31 +0200

    lcd modul issue

--- src/classes/lcd_module.py
+++ src/classes/lcd_module.py
@@ -10,16 +10,16 @@ time_obj = Time()
 # logging.log_info('Module: ' + 'PCF8574' + ' loaded')
 
 
-def __write_row1(text):
-    """ Description """
-    lcd.cursor_pos = (0, 6)
-    lcd.write_string(text)
-
-
-def __write_row2(text):
-    """ Description """
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string(text)
+# def __write_row1(text):
+#     """ Description """
+#     lcd.cursor_pos = (0, 0)
+#     lcd.write_string(text)
+#
+#
+# def __write_row2(text):
+#     """ Description """
+#     lcd.cursor_pos = (1, 0)
+#     lcd.write_string(text)
 
 
 def __write_time():
@@ -30,35 +30,44 @@ def __write_time():
 
 
 def write_lcd(what, show):
-    """ Description """
+    # date_wl = get_date_time('basic')
+    global system_time
 
     if what == 'kapali':
-        __write_row1(u'kapali')
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string(system_time + '    ' + u'kapali')
 
     elif what == 'start':
-        __write_row1(u'calisiyor')
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string(system_time + ' ' + u'calisiyor')
 
     elif what == 'stop':
-        __write_row1(u'duruyor')
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string(system_time + '   ' + u'duruyor')
 
     elif what == 'bobin':
-        __write_row1(u'bobin')
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string(system_time + '     ' + u'bobin')
 
     elif what == 'cozgu':
-        __write_row1(u'cozgu')
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string(system_time + '     ' + u'cozgu')
 
     elif what == 'ariza':
-        __write_row1(u'ariza')
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string(system_time + '     ' + u'ariza')
 
     elif what == 'ayar':
-        __write_row1(u'ayar')
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string(system_time + '      ' + u'ayar')
 
     elif what == 'reset':
-        __write_row2(u'Counter= ' + '0      ')
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string(u'Counter= ' + '0      ')
 
     elif what == 'counter':
-        __write_row2(u'Counter= ' + str(show))
-
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string(u'Counter= ' + str(show))
 
 def sync_time():
     """ Description """

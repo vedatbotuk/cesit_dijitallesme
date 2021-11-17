4f0300974a2a94a0514d43bb00bda3302ff46670
vedat <vedat.botuk@gmail.com>
2021-11-17 23:04:27 +0100

    counter func changed

--- src/classes/os_commands.py
+++ src/classes/os_commands.py
@@ -1,12 +1,21 @@
 from os import system
 from .lcd_module import LcdModule
 from time import sleep
+from .json_funcs import get_setup
+from .log_info import LogInfo
 
 LCD = LcdModule()
+
+config_json = get_setup()
+
+logging = LogInfo(config_json['main']['log'],
+                       config_json['main']['log_level'],
+                       config_json['main']['log_path'])
 
 
 def shutdown_system():
     """100"""
+    logging.log_info('system shutting down...')
     LCD.refresh_lcd('code_shutdown')
     sleep(2)
     LCD.lcd_close()
@@ -16,6 +25,7 @@ def shutdown_system():
 
 def reboot_system():
     """101"""
+    logging.log_info('system rebooting...')
     LCD.refresh_lcd('code_reboot')
     sleep(2)
     LCD.lcd_close()
@@ -25,6 +35,7 @@ def reboot_system():
 
 def restart_program():
     """102"""
+    logging.log_info('program restarting...')
     LCD.refresh_lcd('code_restart_program')
     sleep(2)
     LCD.lcd_close()
@@ -33,6 +44,8 @@ def restart_program():
 
 
 def update_code():
+    """103"""
+    logging.log_info('code updating...')
     LCD.refresh_lcd('code_update')
     system("cd /home/pi/cesit_dijitallesme/ && git pull")
     LCD.lcd_close()

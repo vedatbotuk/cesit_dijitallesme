c1dc1c64ae9e51cd510ccf527d7033561cff684c
vedat <vedat.botuk@gmail.com>
2021-11-30 15:01:12 +0100

    bug fixing

--- src/main.py
+++ src/main.py
@@ -496,15 +496,15 @@ def event_reset(channel):
         RESET_PUSHED = 1
 
     elif btn_rest is False:
-        if RESET_PUSHED == 1:
+        if MACHINE_START == 0 and RESET_PUSHED == 1:
             COUNTER_NR = 0
             TIME_WATCH.reset_time()
             RESET_CHANGED = 1
             RESET_PUSHED = 0
             LOGGING.log_info('Counter reset')
             # LOGGING.log_info(channel)
-        else:
-            LOGGING.log_info('Wrong signal -> Reset was not pushed ' + str(channel))
+        # else:
+        #     LOGGING.log_info('Wrong signal -> Reset was not pushed ' + str(channel))
 
 
 def loop():
@@ -520,7 +520,7 @@ def add_events():
     """ Description """
     BTN_START_STOP.add_callback(mode='both', callback=event_start_stop)
     BTN_COUNTER.add_callback(mode='both', callback=event_counter)
-    BTN_RESET.add_callback(mode='rising', callback=event_reset)
+    BTN_RESET.add_callback(mode='both', callback=event_reset)
 
 
 if __name__ == '__main__':

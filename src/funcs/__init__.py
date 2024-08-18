4bb136f358d7c9925d2956da6567e6b047515cd9
Vedat Botuk <vedat.botuk@gmail.com>
2024-08-18 22:03:25 +0200

    test

---
+++ src/funcs/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+# -*- coding: utf-8 -*-
+""" Description """
+
+from keypad_funcs import *
--- src/main.py
+++ src/main.py
@@ -6,8 +6,9 @@
 # Imports
 from time import sleep
 import classes
-from classes import os_commands
+from funcs import *
 import concurrent.futures
+
 
 is_shutdown = False
 
@@ -65,7 +66,6 @@ TIME_BTW_COUNTER = 0
 
 CHECK_MINUTE = ''
 
-LCD = classes.LcdModule()
 BTN_KAPALI = classes.ButtonSwitch(CONFIG_JSON['switches']['btn_kapali'])
 BTN_KAPALI.add_switches()
 BTN_START_STOP = classes.ButtonSwitch(CONFIG_JSON['switches']['btn_start_stop'])
@@ -83,10 +83,6 @@ BTN_BOBIN.add_switches()
 
 BTN_RESET = classes.ButtonSwitch(CONFIG_JSON['buttons']['btn_reset'])
 BTN_COUNTER = classes.ButtonSwitch(CONFIG_JSON['buttons']['btn_counter'])
-
-KEYPAD_INSTALL = CONFIG_JSON['module']['keypad']['install']
-if KEYPAD_INSTALL is True:
-    KEY_PAD = classes.KeyPad()
 
 
 # end of setup
@@ -296,171 +292,6 @@ def gpio_check_start_stop():
             check_cozgu()
             check_ariza()
             check_ayar()
-
-
-def given_counter():
-    """ Description """
-    return_number = None
-    given_number = ''
-
-    LCD.refresh_lcd('Given_Counter', given_number)
-
-    while True:
-        get_button = str(KEY_PAD.check_button())
-        if get_button == 'C':
-            break
-
-        elif get_button == 'D':
-            given_number = given_number[:-1]
-            LCD.refresh_lcd('Given_Counter', given_number)
-
-        elif get_button == '*':
-            try:
-                return_number = int(given_number)
-
-                LCD.refresh_lcd('successfully', given_number)
-                sleep(2)
-                break
-
-            except Exception as e:
-                LCD.refresh_lcd('Counter_not_allowed')
-                sleep(2)
-                LCD.refresh_lcd('Given_Counter', given_number)
-                return_number = None
-                LOGGING.log_info(e)
-
-        elif get_button in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            given_number = given_number + get_button
-            LCD.refresh_lcd('Given_Counter', given_number)
-
-        sleep(0.2)
-
-    return return_number
-
-
-def keypad_give_total_counter():
-    """ Description """
-    global TOTAL_COUNTER, COUNTER_NR
-
-    if KEYPAD_INSTALL is True:
-        wait = 15
-        checked = 0
-        for cnt in range(0, wait):
-            button_to_give_counter = KEY_PAD.check_button()
-            if button_to_give_counter == "#":
-                checked = checked + 1
-            else:
-                break
-            sleep(0.2)
-
-        if checked == wait:
-            total_counter = given_counter()
-            if total_counter is not None:
-                TOTAL_COUNTER = total_counter
-                JSON_FUNCS.change_json(what='Given_Total_Counter', state=TOTAL_COUNTER)
-        else:
-            pass
-
-
-def keypad_give_os_cmd():
-    """ Description """
-    global COUNTER_NR
-
-    if KEYPAD_INSTALL is True:
-        wait = 15
-        checked = 0
-        for cnt in range(0, wait):
-            button_to_give_total = KEY_PAD.check_button()
-            if button_to_give_total == "D":
-                checked = checked + 1
-            else:
-                break
-            sleep(0.2)
-
-        if checked == wait:
-            given_code = ''
-            LCD.refresh_lcd('Given_Code', given_code)
-
-            while True:
-                get_button = str(KEY_PAD.check_button())
-                if get_button == 'C':
-                    break
-
-                elif get_button == 'D':
-                    given_code = given_code[:-1]
-                    LCD.refresh_lcd('Given_Code', given_code)
-
-                elif get_button == '*':
-
-                    if given_code == '100':
-                        os_commands.shutdown_system()
-                        LCD.lcd_close()
-                        exit()
-                        break
-
-                    elif given_code == '101':
-                        os_commands.reboot_system()
-                        LCD.lcd_close()
-                        exit()
-                        break
-
-                    elif given_code == '102':
-                        os_commands.restart_program()
-                        LCD.lcd_close()
-                        exit()
-                        break
-
-                    elif given_code == '103':
-                        os_commands.update_code()
-                        LCD.lcd_close()
-                        break
-
-                    elif given_code == '104':
-                        change_counter = given_counter()
-                        if change_counter is not None:
-                            COUNTER_NR = change_counter
-                            JSON_FUNCS.change_json(what='Given_Counter', state=COUNTER_NR)
-                        break
-
-                    else:
-                        LCD.refresh_lcd('Code_not_exists')
-                        sleep(2)
-                        given_code = ''
-                        LCD.refresh_lcd('Given_Code', given_code)
-
-                elif get_button in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                    given_code = given_code + get_button
-                    LCD.refresh_lcd('Given_Code', given_code)
-
-                sleep(0.2)
-
-
-def show_total_counter():
-    """ Description """
-    global TOTAL_COUNTER, COUNTER_NR
-
-    if KEYPAD_INSTALL is True:
-        while True:
-            button_to_give_total = KEY_PAD.check_button()
-            if button_to_give_total == 'A':
-                LCD.refresh_lcd(what='show_total', state=TOTAL_COUNTER)
-            else:
-                break
-            sleep(0.5)
-
-
-def show_remainder_counter():
-    """ Description """
-    global TOTAL_COUNTER, COUNTER_NR
-
-    if KEYPAD_INSTALL is True:
-        while True:
-            button_to_give_remainder = KEY_PAD.check_button()
-            if button_to_give_remainder == 'B':
-                LCD.refresh_lcd(what='show_remainder', state=TOTAL_COUNTER - COUNTER_NR)
-            else:
-                break
-            sleep(0.5)
 
 
 def clear_lcd():
